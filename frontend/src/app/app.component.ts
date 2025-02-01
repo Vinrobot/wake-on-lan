0309209956e0397cea306847cbfd1f3a136a3dd3
Vinrobot <vinrobot97@gmail.com>
2025-02-01 16:59:09 +0000

    Add Clarity Design System

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { ClarityModule } from '@clr/angular';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    ClarityModule,
+    RouterOutlet,
+    RouterLink,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'frontend';
 }
--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,11 +1,19 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { ClarityModule } from '@clr/angular';
+import { loadCoreIconSet, loadEssentialIconSet } from '@cds/core/icon';
 
 import { routes } from './app.routes';
+
+loadCoreIconSet();
+loadEssentialIconSet();
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideAnimations(),
+    importProvidersFrom(ClarityModule),
   ],
 };
